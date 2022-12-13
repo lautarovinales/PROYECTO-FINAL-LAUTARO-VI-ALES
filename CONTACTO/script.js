ffb43ec352beb19fd196668dffa9677688b0f515
lautarovinales <118315384+lautarovinales@users.noreply.github.com>
2022-12-12 23:30:39 -0300

    Add files via upload

---
+++ CONTACTO/script.js
@@ -0,0 +1,12 @@
+function inicio(){
+    location.href = "index.html"
+}
+
+function contacto(){
+    location.href = "contacto.html"
+}
+
+function descargas(){
+    location.href = "descargas.html"
+}
+
---
+++ DESCARGAS/script.js
@@ -0,0 +1,16 @@
+function inicio(){
+    location.href = "index.html"
+}
+
+function contacto(){
+    location.href = "contacto.html"
+}
+
+function descargas(){
+    location.href = "descargas.html"
+}
+
+function img(){
+    location.href = "https://i.pinimg.com/736x/40/13/3d/40133de3f92c8419729ace7dfc882c40.jpg"
+}
+
